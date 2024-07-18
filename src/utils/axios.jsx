76bc07a93142a609755dcff5211ced8b7798c602
lokeshdhote <lokeshdhote2004@gmail.com>
2024-07-18 18:08:02 +0530

    second

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000",
+
+
+  // baseURL: "http://localhost:3000",
+  baseURL: "https://internsallaapi.onrender.com/",
   withCredentials:true,
 });
 
